82540c4f41ffe6434a3cd1c67963864f9b469d59
Arsenii Machekhin <arsenium2@gmail.com>
2022-05-21 01:14:26 +0300

    feature: Добавил плавности открытию и закрытию попапов

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,17 @@ const footer = document.querySelector('.footer');
 const fullName = document.querySelector('.profile__heading');
 const job = document.querySelector('.profile__job-title');
 
+// Функция отвечает за открытие попапа редактирования
+// Клонирую шаблон из html и дальше наполняю содержимым
+// Вставляю заполненный шаблон в разметку после футера
+// После вставки заполненного шаблона жду полной загрузки элемента и добавляю плавности
 function getEditPopup() {
   const editPopup = templatePopup.content.cloneNode(true);
   const popupEditHeading = editPopup.querySelector('.popup__heading');
   const popupEditName = editPopup.querySelector('.popup__field_type_name');
   const popupEditJob = editPopup.querySelector('.popup__field_type_job');
   const popupEditSubmit = editPopup.querySelector('.popup__submit');
+  const transition = editPopup.querySelector('.popup');
   popupEditHeading.textContent = 'Редактировать профиль';
   popupEditName.value = fullName.textContent;
   popupEditJob.value = job.textContent;
@@ -20,6 +25,9 @@ function getEditPopup() {
   popupEditJob.placeholder = 'О себе';
   popupEditSubmit.textContent = 'Сохранить';
   footer.after(editPopup);
+  setTimeout(() => {
+    transition.classList.add('smoothness');
+  }, 100);
 };
 
 function getAddPopup() {
@@ -28,21 +36,31 @@ function getAddPopup() {
   const popupEditName = editPopup.querySelector('.popup__field_type_name');
   const popupEditJob = editPopup.querySelector('.popup__field_type_job');
   const popupEditSubmit = editPopup.querySelector('.popup__submit');
+  const transition = editPopup.querySelector('.popup');
   popupEditHeading.textContent = 'Новое место';
   popupEditName.placeholder = 'Название';
   popupEditJob.placeholder = 'Ссылка на картинку';
   popupEditSubmit.textContent = 'Создать';
   footer.after(editPopup);
+  setTimeout(() => {
+    transition.classList.add('smoothness');
+  }, 100);
 };
 
 function closePopupBtn() {
   const popupToClose = document.querySelector('.popup');
+  popupToClose.classList.remove('smoothness');
+  setTimeout(() => {
   popupToClose.remove();
+  }, 1000);
 };
 
 function closeFullPhoto() {
   const photoToClose = document.querySelector('.imagePopup');
+  photoToClose.classList.remove('smoothness');
+  setTimeout(() => {
   photoToClose.remove();
+  }, 1000);
 }
 
 function submitEditPopup(evt) {
@@ -65,7 +83,7 @@ function submitAddUserCard(evt) {
   newCardLink.src = popupEditJob.value;
   newCardLink.alt = popupEditName.value;
   cardsListWrapper.prepend(newCard);
-  closePopupBtn()
+  closePopupBtn();
 };
 
 function likeActive(evt) {
@@ -77,13 +95,22 @@ function openFullPhoto(evt) {
   const imagePopup = templateImagePopup.content.cloneNode(true);
   const photo = imagePopup.querySelector('.imagePopup__photo');
   const heading = imagePopup.querySelector('.imagePopup__heading');
+  const transition = imagePopup.querySelector('.imagePopup');
   const cardPhoto = evt.target;
   photo.src = cardPhoto.src;
   photo.alt = cardPhoto.alt;
   heading.textContent = cardPhoto.alt;
   footer.after(imagePopup);
+  setTimeout(() => {
+    transition.classList.add('smoothness');
+  }, 100);
 };
 
+function removeCard(evt) {
+evt.target.closest('.element').remove();
+}
+
+// Повесил слушатель на всю страницу и на всплытии ловлю целевые элементы и к ним применяю функцию
 page.addEventListener('click', function (evt) {
   const el = evt.target;
   if (el.classList.value === 'profile__edit-button') {
@@ -103,7 +130,10 @@ page.addEventListener('click', function (evt) {
   }
   else if (el.classList.value === 'element__img') {
     openFullPhoto(evt);
-}
+  }
+  else if (el.classList.value === 'element__trash'){
+    removeCard(evt);
+  }
 });
 
 page.addEventListener('submit', function (evt) {
@@ -118,8 +148,8 @@ page.addEventListener('submit', function (evt) {
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Микли (посёлок)',
+    link:   'https://images.unsplash.com/photo-1555948560-27b32a752ff3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387'
   },
   {
     name: 'Челябинская область',
